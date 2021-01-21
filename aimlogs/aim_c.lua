bcbedef7f2f5aec69facad33fb994d067c637972
MaNtouviosGR <57492789+MaNtouviosGR@users.noreply.github.com>
2021-01-21 17:06:21 +0200

    Update aim_c.lua

--- aimlogs/aim_c.lua
+++ aimlogs/aim_c.lua
@@ -1,27 +1,37 @@
-local used = false
+Citizen.CreateThread(function()
+    local lastPedAimed
 
-Citizen.CreateThread(function() --Creates thread
     while true do
-        Citizen.Wait(100)
-        local aiming, targetPed = GetEntityPlayerIsFreeAimingAt(PlayerId(-1))
-        local id = GetPlayerName(PlayerId())
-        if aiming then
+        Citizen.Wait(1000)
+        local letSleep = true
+        local playerPed = PlayerPedId()
+        if DoesEntityExist(playerPed) then
+            if IsPedArmed(playerPed, 4) then
+                letSleep = false
 
-            Citizen.SetTimeout(5000, function()
-                used = false
-            end)
-            
-            if DoesEntityExist(targetPed) and IsEntityAPed(targetPed) then
-                local pedId = GetPlayerServerId(aiming)
-                if used == false then
-                    TriggerServerEvent('aimlogs:log', pedId )
-                    used = true
+                local isAiming, targetPed = GetEntityPlayerIsFreeAimingAt(PlayerId(-1))
+                if isAiming and targetPed ~= lastPedAimed then
+                    lastPedAimed = targetPed
+
+                    if DoesEntityExist(targetPed) and IsEntityAPed(targetPed) then
+                        if IsPedAPlayer(targetPed) then
+                            local targetId = NetworkGetPlayerIndexFromPed(targetPed)
+
+                            if targetId then
+                                local pedId = GetPlayerServerId(targetId)
+                                
+                                if pedId and (pedId > 0) then
+                                    TriggerServerEvent('aimlogs:log', pedId)
+                                end
+                            end
+                        end
+                    end
                 end
-            end 
-        end 
+            end
+        end
 
-        if not aiming then
-            used = false
+        if letSleep then
+            Wait(1000)
         end
     end 
 end)
