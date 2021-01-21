28a8e2daf4c9a3ddf9e96140c8394b6e50ef32c7
MaNtouviosGR <57492789+MaNtouviosGR@users.noreply.github.com>
2021-01-21 17:07:00 +0200

    Update aim_s.lua

--- aimlogs/aim_s.lua
+++ aimlogs/aim_s.lua
@@ -5,5 +5,5 @@ AddEventHandler('aimlogs:log', function(pedId)
     local _source = source
     local name = GetPlayerName(_source)
     local targetName = GetPlayerName(pedId)
-    PerformHttpRequest(hook, function(err, text, headers) end, 'POST', json.encode({embeds={{title="_**Aim Logs**__",description="\nPlayer name: "..name.."\nis aiming: "..targetName.."",color=16711680}}}), { ['Content-Type'] = 'application/json' })
+    PerformHttpRequest(hook, function(err, text, headers) end, 'POST', json.encode({embeds={{title="__**Aim Logs**__",description="\nPlayer name: "..name.. "`[".._source.."]`\nIs aiming: "..targetName.." `["..pedId.."]`",color=16711680}}}), { ['Content-Type'] = 'application/json' })
 end)
